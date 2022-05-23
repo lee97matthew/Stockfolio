d22a92743bf0b7a1fae5861918b5d4c0b861d25d
waffledood <itshaikalhere@hotmail.sg>
2022-05-23 14:24:06 +0800

    Remove commented code of getAutoComplete API call

--- app/src/main/java/com/example/stockfolio/Search.java
+++ app/src/main/java/com/example/stockfolio/Search.java
@@ -30,43 +30,23 @@ public class Search extends AppCompatActivity {
         StocksApi stocksApi = new StocksApi(Search.this);
 
         // click listener for button
-//        btn_findStock.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String ticker = et_dataInput.getText().toString();
-//
-//                stocksApi.getQuote(ticker, new StocksApi.GetQuoteListener() {
-//                    @Override
-//                    public void onError(String message) {
-//                        Toast.makeText(Search.this, message, Toast.LENGTH_LONG).show();
-//                    }
-//
-//                    @Override
-//                    public void onResponse(Stock stock) {
-//                        Toast.makeText(Search.this, String.format("Price of %s: %.2f", ticker, stock.getRegularMarketPrice()), Toast.LENGTH_LONG).show();
-//                        Intent intent = new Intent(Search.this, StockPage.class);
-//                        intent.putExtra("stock", stock);
-//                        startActivity(intent);
-//                    }
-//                });
-//            }
-//        });
-
-        // click listener for button
         btn_findStock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String ticker = et_dataInput.getText().toString();
 
-                stocksApi.getAutoComplete(ticker, new StocksApi.GetAutoCompleteListener() {
+                stocksApi.getQuote(ticker, new StocksApi.GetQuoteListener() {
                     @Override
                     public void onError(String message) {
                         Toast.makeText(Search.this, message, Toast.LENGTH_LONG).show();
                     }
 
                     @Override
-                    public void onResponse(List<Stock> suggestedStocks) {
-                        Toast.makeText(Search.this, "getAutoComplete | success!", Toast.LENGTH_LONG).show();
+                    public void onResponse(Stock stock) {
+                        Toast.makeText(Search.this, String.format("Price of %s: %.2f", ticker, stock.getRegularMarketPrice()), Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(Search.this, StockPage.class);
+                        intent.putExtra("stock", stock);
+                        startActivity(intent);
                     }
                 });
             }
